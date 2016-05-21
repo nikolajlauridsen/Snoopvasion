0fea2df615d9902230d86a9aa13b198f577b31e9
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-05-21 23:12:29 +0200

    Initial commit

---
+++ bullet.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.image = pygame.image.load('images/bullet.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet's position as a decimal value
+        self.y = float(self.rect.y)
+
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.speed_factor
+        # Update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        self.screen.blit(self.image, self.rect)
---
+++ settings.py
@@ -0,0 +1,23 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings. """
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed_factor = 3
+        self.bullets_allowed = 6
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_diretion of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
